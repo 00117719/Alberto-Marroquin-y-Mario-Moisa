8a1149a4666cffbb4ca3f7deeaff489aee9edd33
aljumarmen <aljumarmen>
2020-04-18 15:00:54 -0600

    Add ServicioProfesional class

---
+++ Parcial 1/src/com/AJMM/x00117719/ServicioProfesional.java
@@ -0,0 +1,18 @@
+package com.AJMM.x00117719;
+
+public class ServicioProfesional extends Empleado {
+    private int mesesContrato;
+
+    public ServicioProfesional(String nombre, String puesto, int mesesContrato) {
+        super(nombre, puesto);
+        this.mesesContrato = mesesContrato;
+    }
+
+    public int getMesesContrato() {
+        return mesesContrato;
+    }
+
+    public void setMesesContrato(int mesesContrato) {
+        this.mesesContrato = mesesContrato;
+    }
+}
